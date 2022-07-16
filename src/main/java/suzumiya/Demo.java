eeebd64d26781c4224168ef340919d547a0993b2
SuzumiyaRinko <109336832+SuzumiyaRinko@users.noreply.github.com>
2022-07-16 15:59:32 +0800

    Test
    pull test

--- src/main/java/suzumiya/Demo.java
+++ src/main/java/suzumiya/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("pull Test");
     }
 }
